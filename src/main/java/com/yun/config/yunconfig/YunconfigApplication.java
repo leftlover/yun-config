ab61ef9f50df047afe1005279ac37a2f8609edcf
gavin <qyjava@yeah.net>
2020-08-07 16:30:43 +0800

    config server 初始化

---
+++ src/main/java/com/yun/config/yunconfig/YunconfigApplication.java
@@ -0,0 +1,13 @@
+package com.yun.config.yunconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YunconfigApplication.class, args);
+	}
+
+}
